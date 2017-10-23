90a651447c6bd8ac4797693c6be96aadbd800cc5
Sloan Holzman <sloan.holzman@gmail.com>
2017-10-23 15:31:37 -0400

    create variable, write function createSequence

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  let level = 1;
+  let sequence = [];
+  let userAnswer = [];
+  let firstLight = $("#light1");
+  let secondLight = $("#light2");
+  let thirdLight = $("#light3");
+  let fourthLight = $("#light4");
+  let startButton = $("#start");
+  let delay = 500;
+
+  function createSequence(level) {
+    for (let i = 0; i < level + 2; i++) {
+      let randomSquare = Math.round(Math.random() * 3) + 1;
+      sequence.push(randomSquare);
+    }
+  }
+});
