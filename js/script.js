d250d6e7edd7a2d959f37f435eebe50852f1c8f6
Sloan Holzman <sloan.holzman@gmail.com>
2017-10-23 21:12:26 -0400

    create start button, allow user to enter answer

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ console.log("hello world!");
 let level = 1;
 let sequence = [];
 let userAnswer = [];
+let lights = $(".lights");
 let firstLight = $("#light1");
 let secondLight = $("#light2");
 let thirdLight = $("#light3");
 let fourthLight = $("#light4");
 let startButton = $("#start");
 let delay = 1000;
+let userTurn = false;
 
 function createSequence(level) {
-  for (let i = 0; i < level + 2; i++) {
+  for (let i = 0; i < level + 3; i++) {
     let randomSquare = Math.round(Math.random() * 3) + 1;
     sequence.push(randomSquare);
   }
@@ -20,7 +22,7 @@ function createSequence(level) {
 var i = 0;
 
 // (2) define the delayed loop function
-function delayedLoop(sequence) {
+function loopSequence(sequence) {
   setTimeout(function() {
     // (3) do action
     console.log(`iteration ${i}`);
@@ -30,9 +32,10 @@ function delayedLoop(sequence) {
 
     // (4) if the end of the array has been reached, stop
     if (++i >= sequence.length) {
+      userTurn = true;
       return;
     }
-    delayedLoop(sequence);
+    loopSequence(sequence);
   }, delay);
 
   // (5) recursively call the delayed loop function with a delay
@@ -64,9 +67,51 @@ function checkLight(light) {
 function flashLight(light) {
   light.css("opacity", 1);
   setTimeout(function() {
+    console.log("change");
     light.css("opacity", 0.2);
-  }, delay);
+  }, 500);
 }
 
-createSequence(5);
-console.log(sequence);
+//need to click for the parent and then if child contains...but this works for now
+firstLight.click(function(e) {
+  e.preventDefault();
+  if (userAnswer.length < sequence.length && userTurn) {
+    userAnswer.push(1);
+    console.log(userAnswer);
+    flashLight(firstLight);
+  }
+});
+
+secondLight.click(function(e) {
+  e.preventDefault();
+  if (userAnswer.length < sequence.length && userTurn) {
+    userAnswer.push(2);
+    console.log(userAnswer);
+    flashLight(secondLight);
+  }
+});
+
+thirdLight.click(function(e) {
+  e.preventDefault();
+  if (userAnswer.length < sequence.length && userTurn) {
+    userAnswer.push(3);
+    console.log(userAnswer);
+    flashLight(thirdLight);
+  }
+});
+
+fourthLight.click(function(e) {
+  e.preventDefault();
+  if (userAnswer.length < sequence.length && userTurn) {
+    userAnswer.push(4);
+    console.log(userAnswer);
+    flashLight(fourthLight);
+  }
+});
+
+startButton.click(function(e) {
+  e.preventDefault();
+  console.log("clicked");
+  createSequence(level);
+  loopSequence(sequence);
+});
